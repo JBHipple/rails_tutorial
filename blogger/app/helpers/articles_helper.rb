8b60132250d7248823ad5d2b6ca16545760aedff
Joshua Hipple <jbhipple@gmail.com>
2016-05-08 04:29:50 -0500

    Moved files for Heroku deployment

--- blogger/app/helpers/articles_helper.rb
+++
@@ -1,7 +0,0 @@
-module ArticlesHelper
-
-	def article_params
-		params.require(:article).permit(:title, :body, :tag_list)
-	end
-	
-end
--- blogger/app/views/authors/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @author, :id, :username, :email, :password, :password_confirm, :created_at, :updated_at
